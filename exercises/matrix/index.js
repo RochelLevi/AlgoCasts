21846af8b378bfda470c1ae2488e1e25d69c5044
RochelLevi <rochlevi@gmail.com>
2018-04-30 11:40:32 -0400

    solved matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,60 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let spiral = []
+  for (i = 0; i < n; i++){
+    spiral.push([])
+  }
+  let direction = "right"
+  let row = 0
+  let col = -1
+
+  for (i = 1; i <= n*n; i++){
+    switch (direction){
+      case "right":
+        if (col === n - 1 || spiral[row][col + 1]){
+          direction = "down"
+          spiral[row + 1][col] = i
+          row++
+        } else{
+          spiral[row][col + 1] = i
+          col++
+        }
+        break;
+      case "left":
+        if (col === 0 || spiral[row][col - 1]){
+          direction = "up"
+          spiral[row - 1][col] = i
+          row--
+        } else{
+          spiral[row][col - 1] = i
+          col--
+        }
+        break;
+      case "up":
+        if (row === 0 || spiral[row - 1][col]){
+          direction = "right"
+          spiral[row][col + 1] = i
+          col++
+        } else{
+          spiral[row - 1][col] = i
+          row--
+        }
+        break;
+      case "down":
+        if (row === n - 1 || spiral[row + 1][col]){
+          direction = "left"
+          spiral[row][col - 1] = i
+          col--
+        } else{
+          spiral[row + 1][col] = i
+          row++
+        }
+        break;
+    }
+  }
+  return spiral
+}
 
 module.exports = matrix;
